84002340f04ef65dc6973ee667c226ee581e6000
Ricardo Niepel <ricardo.niepel@outlook.com>
2019-12-23 00:59:42 +0000

    day9 go

---
+++ go/src/day9/day9.go
@@ -0,0 +1,404 @@
+package day9
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func getAmplifierSeriesWithFeedbackLoopMaxThrusterSignal(intcode string) (phaseSetting string, thrusterSignal string) {
+	possiblePhaseSettings := []string{"5", "6", "7", "8", "9"}
+
+	var currentPhaseSetting []string
+
+	for _, a := range possiblePhaseSettings {
+		currentPhaseSetting = []string{a}
+
+		for _, b := range possiblePhaseSettings {
+			if contains(&currentPhaseSetting, b) {
+				continue
+			}
+			currentPhaseSetting = append(currentPhaseSetting, b)
+
+			for _, c := range possiblePhaseSettings {
+				if contains(&currentPhaseSetting, c) {
+					continue
+				}
+				currentPhaseSetting = append(currentPhaseSetting, c)
+
+				for _, d := range possiblePhaseSettings {
+					if contains(&currentPhaseSetting, d) {
+						continue
+					}
+					currentPhaseSetting = append(currentPhaseSetting, d)
+
+					for _, e := range possiblePhaseSettings {
+						if contains(&currentPhaseSetting, e) {
+							continue
+						}
+						currentPhaseSetting = append(currentPhaseSetting, e)
+
+						thrusterSignalTmp := runAmplifierSeries(intcode, currentPhaseSetting...)
+						tst, _ := strconv.Atoi(thrusterSignalTmp)
+						ts, _ := strconv.Atoi(thrusterSignal)
+						if tst > ts {
+							phaseSetting = strings.Join(currentPhaseSetting, ",")
+							thrusterSignal = thrusterSignalTmp
+						}
+
+						remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+					}
+					remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+				}
+				remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+			}
+			remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+		}
+	}
+
+	return
+}
+
+func getAmplifierSeriesMaxThrusterSignal(intcode string) (phaseSetting string, thrusterSignal string) {
+	possiblePhaseSettings := []string{"0", "1", "2", "3", "4"}
+
+	var currentPhaseSetting []string
+
+	for _, a := range possiblePhaseSettings {
+		currentPhaseSetting = []string{a}
+
+		for _, b := range possiblePhaseSettings {
+			if contains(&currentPhaseSetting, b) {
+				continue
+			}
+			currentPhaseSetting = append(currentPhaseSetting, b)
+
+			for _, c := range possiblePhaseSettings {
+				if contains(&currentPhaseSetting, c) {
+					continue
+				}
+				currentPhaseSetting = append(currentPhaseSetting, c)
+
+				for _, d := range possiblePhaseSettings {
+					if contains(&currentPhaseSetting, d) {
+						continue
+					}
+					currentPhaseSetting = append(currentPhaseSetting, d)
+
+					for _, e := range possiblePhaseSettings {
+						if contains(&currentPhaseSetting, e) {
+							continue
+						}
+						currentPhaseSetting = append(currentPhaseSetting, e)
+
+						thrusterSignalTmp := runAmplifierSeries(intcode, currentPhaseSetting...)
+						tst, _ := strconv.Atoi(thrusterSignalTmp)
+						ts, _ := strconv.Atoi(thrusterSignal)
+						if tst > ts {
+							phaseSetting = strings.Join(currentPhaseSetting, ",")
+							thrusterSignal = thrusterSignalTmp
+						}
+
+						remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+					}
+					remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+				}
+				remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+			}
+			remove(&currentPhaseSetting, len(currentPhaseSetting)-1)
+		}
+	}
+
+	return
+}
+
+func runAmplifierSeries(intcode string, phaseSetting ...string) (thrusterSignal string) {
+
+	ampA := newIntcodeProgram(intcode)
+	ampB := newIntcodeProgram(intcode)
+	ampC := newIntcodeProgram(intcode)
+	ampD := newIntcodeProgram(intcode)
+	ampE := newIntcodeProgram(intcode)
+
+	inputAChan := make(chan string, 2)
+	outputAChan := make(chan string, 1)
+	outputBChan := make(chan string, 1)
+	outputCChan := make(chan string, 1)
+	outputDChan := make(chan string, 1)
+	doneA := make(chan bool, 1)
+	doneB := make(chan bool, 1)
+	doneC := make(chan bool, 1)
+	doneD := make(chan bool, 1)
+	doneE := make(chan bool, 1)
+
+	inputAChan <- phaseSetting[0]
+	inputAChan <- "0"
+	outputAChan <- phaseSetting[1]
+	outputBChan <- phaseSetting[2]
+	outputCChan <- phaseSetting[3]
+	outputDChan <- phaseSetting[4]
+
+	go ampA.execute(inputAChan, outputAChan, doneA)
+	go ampB.execute(outputAChan, outputBChan, doneB)
+	go ampC.execute(outputBChan, outputCChan, doneC)
+	go ampD.execute(outputCChan, outputDChan, doneD)
+	go ampE.execute(outputDChan, inputAChan, doneE)
+
+	<-doneE
+	return <-inputAChan
+}
+
+func run(intcode string, inputs ...string) (finalIntcode string, output string) {
+	intcodeProgram := newIntcodeProgram(intcode)
+
+	inputChan := make(chan string)
+	for _, i := range inputs {
+		go func() { inputChan <- i }()
+	}
+
+	outputChan := make(chan string, 100)
+	done := make(chan bool, 1)
+	go intcodeProgram.execute(inputChan, outputChan, done)
+	<-done
+
+	var outputs []string
+	for len(outputChan) > 0 {
+		outputs = append(outputs, <-outputChan)
+	}
+	output = strings.Join(outputs, "\n")
+
+	finalIntcode = intcodeProgram.currentProgram
+	return
+}
+
+func runWithNounVerb(intcode string, noun int, verb int, inputs ...string) (finalIntcode string, output string) {
+	intcodeProgram := newIntcodeProgramWithNounVerb(intcode, noun, verb)
+
+	inputChan := make(chan string)
+	for _, i := range inputs {
+		go func() { inputChan <- i }()
+	}
+
+	outputChan := make(chan string, 100)
+	done := make(chan bool, 1)
+	go intcodeProgram.execute(inputChan, outputChan, done)
+	<-done
+
+	var outputs []string
+	for len(outputChan) > 0 {
+		outputs = append(outputs, <-outputChan)
+	}
+	output = strings.Join(outputs, "\n")
+
+	finalIntcode = intcodeProgram.currentProgram
+	return
+}
+
+type ParameterMode int
+
+const (
+	PositionMode  ParameterMode = 0
+	ImmediateMode ParameterMode = 1
+	RelativeMode  ParameterMode = 2
+)
+
+type IntcodeProgram struct {
+	initialProgram string
+	currentProgram string
+
+	positions    []string
+	relativeBase int
+}
+
+func newIntcodeProgram(program string) *IntcodeProgram {
+	return newIntcodeProgramWithNounVerb(program, -1, -1)
+}
+
+func newIntcodeProgramWithNounVerb(program string, noun int, verb int) *IntcodeProgram {
+	positions := strings.Split(program, ",")
+	if noun != -1 {
+		if noun < 0 || noun > 99 {
+			panic("noun out of range 0-99")
+		}
+		positions[1] = fmt.Sprint(noun)
+	}
+	if verb != -1 {
+		if verb < 0 || verb > 99 {
+			panic("verb out of range 0-99")
+		}
+		positions[2] = fmt.Sprint(verb)
+	}
+	program = strings.Join(positions, ",")
+
+	instance := IntcodeProgram{initialProgram: program, relativeBase: 0}
+	instance.positions = strings.Split(program, ",")
+	return &instance
+}
+
+func toParameterMode(modeBit byte) ParameterMode {
+	tmp, _ := strconv.Atoi(string(modeBit))
+	return ParameterMode(tmp)
+}
+
+func (ip *IntcodeProgram) execute(input <-chan string, output chan<- string, done chan<- bool) {
+	currentPosition := 0
+
+	for {
+		opcode := ip.positions[currentPosition]
+		originalOpcode := opcode
+
+		modeParam1 := PositionMode
+		modeParam2 := PositionMode
+		modeParam3 := PositionMode
+		if len(opcode) > 2 {
+			opcode = originalOpcode[len(opcode)-2:]
+			modeBits := reverse(originalOpcode[:len(originalOpcode)-2])
+
+			if len(modeBits) > 2 {
+				modeParam3 = toParameterMode(modeBits[2])
+			}
+			if len(modeBits) > 1 {
+				modeParam2 = toParameterMode(modeBits[1])
+			}
+			if len(modeBits) > 0 {
+				modeParam1 = toParameterMode(modeBits[0])
+			}
+		}
+
+		if opcode == "99" {
+			break
+		}
+
+		switch {
+		case opcode == "1" || opcode == "01":
+			firstOperant := ip.getParameterRead(currentPosition+1, modeParam1)
+			secondOperant := ip.getParameterRead(currentPosition+2, modeParam2)
+			storePosition := ip.getParameterWrite(currentPosition+3, modeParam3)
+			*storePosition = fmt.Sprint(firstOperant + secondOperant)
+			currentPosition += 4
+		case opcode == "2" || opcode == "02":
+			firstOperant := ip.getParameterRead(currentPosition+1, modeParam1)
+			secondOperant := ip.getParameterRead(currentPosition+2, modeParam2)
+			storePosition := ip.getParameterWrite(currentPosition+3, modeParam3)
+			*storePosition = fmt.Sprint(firstOperant * secondOperant)
+			currentPosition += 4
+		case opcode == "3" || opcode == "03": // input
+			storePosition := ip.getParameterWrite(currentPosition+1, modeParam1)
+			*storePosition = <-input
+			currentPosition += 2
+		case opcode == "4" || opcode == "04": // ouput
+			firstOperant := ip.getParameterRead(currentPosition+1, modeParam1)
+			output <- fmt.Sprint(firstOperant)
+			currentPosition += 2
+		case opcode == "5" || opcode == "05": // jump-if-true
+			firstParam := ip.getParameterRead(currentPosition+1, modeParam1)
+			secParam := ip.getParameterRead(currentPosition+2, modeParam2)
+			if firstParam != 0 {
+				currentPosition = secParam
+			} else {
+				currentPosition += 3
+			}
+		case opcode == "6" || opcode == "06": // jump-if-false
+			firstParam := ip.getParameterRead(currentPosition+1, modeParam1)
+			secParam := ip.getParameterRead(currentPosition+2, modeParam2)
+			if firstParam == 0 {
+				currentPosition = secParam
+			} else {
+				currentPosition += 3
+			}
+		case opcode == "7" || opcode == "07": // less than
+			firstParam := ip.getParameterRead(currentPosition+1, modeParam1)
+			secParam := ip.getParameterRead(currentPosition+2, modeParam2)
+			storePosition := ip.getParameterWrite(currentPosition+3, modeParam3)
+
+			if firstParam < secParam {
+				*storePosition = "1"
+			} else {
+				*storePosition = "0"
+			}
+			currentPosition += 4
+		case opcode == "8" || opcode == "08": // equals
+			firstParam := ip.getParameterRead(currentPosition+1, modeParam1)
+			secParam := ip.getParameterRead(currentPosition+2, modeParam2)
+			storePosition := ip.getParameterWrite(currentPosition+3, modeParam3)
+
+			if firstParam == secParam {
+				*storePosition = "1"
+			} else {
+				*storePosition = "0"
+			}
+			currentPosition += 4
+		case opcode == "9" || opcode == "09": // adjusts the relative base
+			firstParam := ip.getParameterRead(currentPosition+1, modeParam1)
+			ip.relativeBase += firstParam
+			currentPosition += 2
+		default:
+			panic("unknown opcode:" + originalOpcode)
+		}
+	}
+
+	ip.currentProgram = strings.Join(ip.positions, ",")
+
+	done <- true
+}
+
+func (ip *IntcodeProgram) getParameterRead(position int, mode ParameterMode) int {
+	var parameter int
+
+	switch mode {
+	case PositionMode:
+		lookupPosition, _ := strconv.Atoi(ip.positions[position])
+		ensureMemory(&ip.positions, lookupPosition)
+		parameter, _ = strconv.Atoi(ip.positions[lookupPosition])
+	case RelativeMode:
+		relativeLookupPosition, _ := strconv.Atoi(ip.positions[position])
+		ensureMemory(&ip.positions, ip.relativeBase+relativeLookupPosition)
+		parameter, _ = strconv.Atoi(ip.positions[ip.relativeBase+relativeLookupPosition])
+	case ImmediateMode:
+		parameter, _ = strconv.Atoi(ip.positions[position])
+	}
+
+	return parameter
+}
+
+func (ip *IntcodeProgram) getParameterWrite(position int, mode ParameterMode) *string {
+	switch mode {
+	case PositionMode:
+		lookupPosition, _ := strconv.Atoi(ip.positions[position])
+		ensureMemory(&ip.positions, lookupPosition)
+		return &ip.positions[lookupPosition]
+	case RelativeMode:
+		relativeLookupPosition, _ := strconv.Atoi(ip.positions[position])
+		ensureMemory(&ip.positions, ip.relativeBase+relativeLookupPosition)
+		return &ip.positions[ip.relativeBase+relativeLookupPosition]
+	}
+
+	panic("not supported " + fmt.Sprint(position) + " " + fmt.Sprint(mode))
+}
+
+func ensureMemory(positions *[]string, index int) {
+	if !(index < len(*positions)) {
+		*positions = append(*positions, make([]string, index+1-len(*positions))...)
+	}
+}
+
+func contains(arr *[]string, elem string) bool {
+	for _, e := range *arr {
+		if e == elem {
+			return true
+		}
+	}
+
+	return false
+}
+
+func remove(arr *[]string, index int) {
+	(*arr) = append((*arr)[:index], (*arr)[index+1:]...)
+}
+
+func reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
+}
---
+++ go/src/day9/day9_test.go
@@ -0,0 +1,283 @@
+package day9
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_partone_small(t *testing.T) {
+	tests := []struct {
+		inputIntcode    string
+		input           string
+		expectedIntcode string
+		expectedOutput  string
+	}{
+		{
+			"109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99",
+			"",
+			"109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,16,1",
+			"109\n1\n204\n-1\n1001\n100\n1\n100\n1008\n100\n16\n101\n1006\n101\n0\n99",
+		},
+		{
+			"1102,34915192,34915192,7,4,7,99,0",
+			"",
+			"1102,34915192,34915192,7,4,7,99,1219070632396864",
+			"1219070632396864",
+		},
+		{
+			"104,1125899906842624,99",
+			"",
+			"104,1125899906842624,99",
+			"1125899906842624",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.input), func(t *testing.T) {
+			result, output := run(tt.inputIntcode, tt.input)
+			assert.Equal(t, tt.expectedIntcode, result)
+			assert.Equal(t, tt.expectedOutput, output)
+		})
+	}
+}
+
+func Test_partone_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputIntcode := string(dat)
+	_, output := run(inputIntcode, "1")
+
+	assert.Equal(t, "2594708277", output)
+}
+
+func Test_parttwo_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputIntcode := string(dat)
+	_, output := run(inputIntcode, "2")
+
+	assert.Equal(t, "87721", output)
+}
+
+func Test_day7_partone_small(t *testing.T) {
+	tests := []struct {
+		inputIntcode              string
+		expectedPhaseSetting      string
+		expectedMaxThrusterSignal string
+	}{
+		{
+			"3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
+			"4,3,2,1,0",
+			"43210",
+		},
+		{
+			"3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+			"0,1,2,3,4",
+			"54321",
+		},
+		{
+			"3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+			"1,0,4,3,2",
+			"65210",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.inputIntcode), func(t *testing.T) {
+			phaseSetting, thrusterSignal := getAmplifierSeriesMaxThrusterSignal(tt.inputIntcode)
+			assert.Equal(t, tt.expectedPhaseSetting, phaseSetting)
+			assert.Equal(t, tt.expectedMaxThrusterSignal, thrusterSignal)
+		})
+	}
+}
+
+func Test_day7_partone_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day7.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	phaseSetting, thrusterSignal := getAmplifierSeriesMaxThrusterSignal(input)
+
+	assert.Equal(t, "0,2,4,3,1", phaseSetting)
+	assert.Equal(t, "567045", thrusterSignal)
+}
+
+func Test_day7_parttwo_small(t *testing.T) {
+	tests := []struct {
+		inputIntcode              string
+		expectedPhaseSetting      string
+		expectedMaxThrusterSignal string
+	}{
+		{
+			"3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
+			"9,8,7,6,5",
+			"139629729",
+		},
+		{
+			"3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
+			"9,7,8,5,6",
+			"18216",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.inputIntcode), func(t *testing.T) {
+			phaseSetting, thrusterSignal := getAmplifierSeriesWithFeedbackLoopMaxThrusterSignal(tt.inputIntcode)
+			assert.Equal(t, tt.expectedPhaseSetting, phaseSetting)
+			assert.Equal(t, tt.expectedMaxThrusterSignal, thrusterSignal)
+		})
+	}
+}
+
+func Test_day7_parttwo_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day7.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	phaseSetting, thrusterSignal := getAmplifierSeriesWithFeedbackLoopMaxThrusterSignal(input)
+
+	assert.Equal(t, "6,5,7,8,9", phaseSetting)
+	assert.Equal(t, "39016654", thrusterSignal)
+}
+
+func Test_day5_partone_small(t *testing.T) {
+	tests := []struct {
+		inputIntcode    string
+		input           string
+		expectedIntcode string
+		expectedOutput  string
+	}{
+		{
+			"3,0,4,0,99",
+			"9876",
+			"9876,0,4,0,99",
+			"9876",
+		},
+		{
+			"1002,4,3,4,33",
+			"",
+			"1002,4,3,4,99",
+			"",
+		},
+		{
+			"1101,100,-1,4,0",
+			"",
+			"1101,100,-1,4,99",
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.input), func(t *testing.T) {
+			result, output := run(tt.inputIntcode, tt.input)
+			assert.Equal(t, tt.expectedIntcode, result)
+			assert.Equal(t, tt.expectedOutput, output)
+		})
+	}
+}
+
+func Test_day5_partone_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day5.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	_, output := run(input, "1")
+
+	expected := "0\n0\n0\n0\n0\n0\n0\n0\n0\n15386262"
+	assert.Equal(t, expected, output)
+}
+
+func Test_day5_parttwo_small(t *testing.T) {
+	tests := []struct {
+		inputIntcode    string
+		input           string
+		expectedIntcode string
+		expectedOutput  string
+	}{
+		{
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"7",
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,7,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"999",
+		},
+		{
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"8",
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,1000,8,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"1000",
+		},
+		{
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"9",
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,1001,9,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			"1001",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.input), func(t *testing.T) {
+			result, output := run(tt.inputIntcode, tt.input)
+			assert.Equal(t, tt.expectedIntcode, result)
+			assert.Equal(t, tt.expectedOutput, output)
+		})
+	}
+}
+
+func Test_day5_parttwo_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day5.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	_, output := run(input, "5")
+
+	expected := "10376124"
+	assert.Equal(t, expected, output)
+}
+
+func Test_day2_partone_small(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedOutput string
+	}{
+		{"1,0,0,0,99", "2,0,0,0,99"},
+		{"2,3,0,3,99", "2,3,0,6,99"},
+		{"2,4,4,5,99,0", "2,4,4,5,99,9801"},
+		{"1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.input), func(t *testing.T) {
+			result, _ := run(tt.input, "")
+			assert.Equal(t, tt.expectedOutput, result)
+		})
+	}
+}
+
+func Test_day2_partone_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day2.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	result, _ := runWithNounVerb(input, 12, 2, "")
+
+	expected := "5534943,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,1,19,9,27,1,23,9,30,1,10,27,34,1,13,31,39,1,35,10,43,2,39,9,129,1,43,13,134,1,5,47,135,1,6,51,137,1,13,55,142,1,59,6,144,1,63,10,148,2,67,6,296,1,71,5,297,2,75,10,1188,1,79,6,1190,1,83,5,1191,1,87,6,1193,1,91,13,1198,1,95,6,1200,2,99,10,4800,1,103,6,4802,2,6,107,9604,1,13,111,9609,2,115,10,38436,1,119,5,38437,2,10,123,153748,2,127,9,461244,1,5,131,461245,2,10,135,1844980,2,139,9,5534940,1,143,2,5534942,1,5,147,0,99,2,0,14,0"
+	assert.Equal(t, expected, result)
+}
+
+func Test_day2_parttwo_main(t *testing.T) {
+	dat, err := ioutil.ReadFile("input_day2.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	result, _ := runWithNounVerb(input, 76, 3, "")
+
+	expected := "19690720,76,3,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,152,1,19,9,155,1,23,9,158,1,10,27,162,1,13,31,167,1,35,10,171,2,39,9,513,1,43,13,518,1,5,47,519,1,6,51,521,1,13,55,526,1,59,6,528,1,63,10,532,2,67,6,1064,1,71,5,1065,2,75,10,4260,1,79,6,4262,1,83,5,4263,1,87,6,4265,1,91,13,4270,1,95,6,4272,2,99,10,17088,1,103,6,17090,2,6,107,34180,1,13,111,34185,2,115,10,136740,1,119,5,136741,2,10,123,546964,2,127,9,1640892,1,5,131,1640893,2,10,135,6563572,2,139,9,19690716,1,143,2,19690719,1,5,147,0,99,2,0,14,0"
+	assert.Equal(t, expected, result)
+}
